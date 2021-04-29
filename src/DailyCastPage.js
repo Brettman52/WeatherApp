093a44d91fc44cff665e5e5070b5e99b41da5296
Brett Adams <Bretman52@yahoo.com>
2021-04-29 12:00:48 -0500

    reworked image size

--- src/DailyCastPage.js
+++ src/DailyCastPage.js
@@ -2,6 +2,12 @@ import React, {Component} from 'react'
 import styled from 'styled-components'
 
 const DickButt = styled.img `
+
+@media (max-width: 768px) {
+    height: 350px;
+    width: 350px;
+ 
+}
     height: 1000px;
     width: 1000px;
     margin-left: auto;
